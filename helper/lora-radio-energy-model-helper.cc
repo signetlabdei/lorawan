673d91479a29496e5e9dfe4426404a124aa17f5e
DvdMgr <magrin.davide@gmail.com>
2017-10-24 12:48:12 +0200

    Add first energy model support

---
+++ helper/lora-radio-energy-model-helper.cc
@@ -0,0 +1,106 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Romagnolo Stefano <romagnolostefano93@gmail.com>
+ */
+
+#include "lora-radio-energy-model-helper.h"
+#include "ns3/lora-net-device.h"
+#include "ns3/lora-tx-current-model.h"
+#include "ns3/end-device-lora-phy.h"
+
+namespace ns3 {
+
+LoraRadioEnergyModelHelper::LoraRadioEnergyModelHelper ()
+{
+  m_radioEnergy.SetTypeId ("ns3::LoraRadioEnergyModel");
+}
+
+LoraRadioEnergyModelHelper::~LoraRadioEnergyModelHelper ()
+{
+}
+
+void
+LoraRadioEnergyModelHelper::Set (std::string name, const AttributeValue &v)
+{
+  m_radioEnergy.Set (name, v);
+}
+
+void
+LoraRadioEnergyModelHelper::SetTxCurrentModel (std::string name,
+                                               std::string n0, const AttributeValue& v0,
+                                               std::string n1, const AttributeValue& v1,
+                                               std::string n2, const AttributeValue& v2,
+                                               std::string n3, const AttributeValue& v3,
+                                               std::string n4, const AttributeValue& v4,
+                                               std::string n5, const AttributeValue& v5,
+                                               std::string n6, const AttributeValue& v6,
+                                               std::string n7, const AttributeValue& v7)
+{
+  ObjectFactory factory;
+  factory.SetTypeId (name);
+  factory.Set (n0, v0);
+  factory.Set (n1, v1);
+  factory.Set (n2, v2);
+  factory.Set (n3, v3);
+  factory.Set (n4, v4);
+  factory.Set (n5, v5);
+  factory.Set (n6, v6);
+  factory.Set (n7, v7);
+  m_txCurrentModel = factory;
+}
+
+
+/*
+ * Private function starts here.
+ */
+
+Ptr<DeviceEnergyModel>
+LoraRadioEnergyModelHelper::DoInstall (Ptr<NetDevice> device,
+                                       Ptr<EnergySource> source) const
+{
+  NS_ASSERT (device != NULL);
+  NS_ASSERT (source != NULL);
+  // check if device is LoraNetDevice
+  std::string deviceName = device->GetInstanceTypeId ().GetName ();
+  if (deviceName.compare ("ns3::LoraNetDevice") != 0)
+    {
+      NS_FATAL_ERROR ("NetDevice type is not LoraNetDevice!");
+    }
+  Ptr<Node> node = device->GetNode ();
+  Ptr<LoraRadioEnergyModel> model = m_radioEnergy.Create ()->GetObject<LoraRadioEnergyModel> ();
+  NS_ASSERT (model != NULL);
+  // set energy source pointer
+  model->SetEnergySource (source);
+
+  // set energy depletion callback
+  // if none is specified, make a callback to EndDeviceLoraPhy::SetSleepMode
+  Ptr<LoraNetDevice> loraDevice = device->GetObject<LoraNetDevice> ();
+  Ptr<EndDeviceLoraPhy> loraPhy = loraDevice->GetPhy ()->GetObject<EndDeviceLoraPhy> ();
+  // add model to device model list in energy source
+  source->AppendDeviceEnergyModel (model);
+  // create and register energy model phy listener
+  loraPhy->RegisterListener (model->GetPhyListener ());
+
+  if (m_txCurrentModel.GetTypeId ().GetUid ())
+    {
+      Ptr<LoraTxCurrentModel> txcurrent = m_txCurrentModel.Create<LoraTxCurrentModel> ();
+      model->SetTxCurrentModel (txcurrent);
+    }
+  return model;
+}
+
+} // namespace ns3
---
+++ model/lora-tx-current-model.cc
@@ -0,0 +1,180 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Author: Romagnolo Stefano <romagnolostefano93@gmail.com>
+ */
+
+#include "lora-tx-current-model.h"
+#include "ns3/log.h"
+#include "ns3/double.h"
+#include "lora-utils.h"
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("LoraTxCurrentModel");
+
+NS_OBJECT_ENSURE_REGISTERED (LoraTxCurrentModel);
+
+TypeId
+LoraTxCurrentModel::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::LoraTxCurrentModel")
+    .SetParent<Object> ()
+    .SetGroupName ("Lora")
+  ;
+  return tid;
+}
+
+LoraTxCurrentModel::LoraTxCurrentModel()
+{
+}
+
+LoraTxCurrentModel::~LoraTxCurrentModel()
+{
+}
+
+// Similarly to the wifi case
+NS_OBJECT_ENSURE_REGISTERED (LinearLoraTxCurrentModel);
+
+TypeId
+LinearLoraTxCurrentModel::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::LinearLoraTxCurrentModel")
+    .SetParent<LoraTxCurrentModel> ()
+    .SetGroupName ("Lora")
+    .AddConstructor<LinearLoraTxCurrentModel> ()
+    .AddAttribute ("Eta", "The efficiency of the power amplifier.",
+                   DoubleValue (0.10),
+                   MakeDoubleAccessor (&LinearLoraTxCurrentModel::SetEta,
+                                       &LinearLoraTxCurrentModel::GetEta),
+                   MakeDoubleChecker<double> ())
+    .AddAttribute ("Voltage", "The supply voltage (in Volts).",
+                   DoubleValue (3.3),
+                   MakeDoubleAccessor (&LinearLoraTxCurrentModel::SetVoltage,
+                                       &LinearLoraTxCurrentModel::GetVoltage),
+                   MakeDoubleChecker<double> ())
+    .AddAttribute ("StandbyCurrent", "The current in the STANDBY state (in Watts).",
+                   DoubleValue (0.0014),  // idle mode = 1.4mA
+                   MakeDoubleAccessor (&LinearLoraTxCurrentModel::SetStandbyCurrent,
+                                       &LinearLoraTxCurrentModel::GetStandbyCurrent),
+                   MakeDoubleChecker<double> ())
+  ;
+  return tid;
+}
+
+LinearLoraTxCurrentModel::LinearLoraTxCurrentModel ()
+{
+  NS_LOG_FUNCTION (this);
+}
+
+LinearLoraTxCurrentModel::~LinearLoraTxCurrentModel()
+{
+  NS_LOG_FUNCTION (this);
+}
+
+void
+LinearLoraTxCurrentModel::SetEta (double eta)
+{
+  NS_LOG_FUNCTION (this << eta);
+  m_eta = eta;
+}
+
+void
+LinearLoraTxCurrentModel::SetVoltage (double voltage)
+{
+  NS_LOG_FUNCTION (this << voltage);
+  m_voltage = voltage;
+}
+
+void
+LinearLoraTxCurrentModel::SetStandbyCurrent (double idleCurrent)
+{
+  NS_LOG_FUNCTION (this << idleCurrent);
+  m_idleCurrent = idleCurrent;
+}
+
+double
+LinearLoraTxCurrentModel::GetEta (void) const
+{
+  return m_eta;
+}
+
+double
+LinearLoraTxCurrentModel::GetVoltage (void) const
+{
+  return m_voltage;
+}
+
+double
+LinearLoraTxCurrentModel::GetStandbyCurrent (void) const
+{
+  return m_idleCurrent;
+}
+
+double
+LinearLoraTxCurrentModel::CalcTxCurrent (double txPowerDbm) const
+{
+  NS_LOG_FUNCTION (this << txPowerDbm);
+  return DbmToW (txPowerDbm) / (m_voltage * m_eta) + m_idleCurrent;
+}
+
+
+NS_OBJECT_ENSURE_REGISTERED (ConstantLoraTxCurrentModel);
+
+TypeId
+ConstantLoraTxCurrentModel::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::ConstantLoraTxCurrentModel")
+    .SetParent<LoraTxCurrentModel> ()
+    .SetGroupName ("Lora")
+    .AddConstructor<ConstantLoraTxCurrentModel> ()
+    .AddAttribute ("TxCurrent",
+                   "The radio Tx current in Ampere.",
+                   DoubleValue (0.028),    // transmit at 0dBm = 28mA
+                   MakeDoubleAccessor (&ConstantLoraTxCurrentModel::SetTxCurrent,
+                                       &ConstantLoraTxCurrentModel::GetTxCurrent),
+                   MakeDoubleChecker<double> ())
+  ;
+  return tid;
+}
+
+ConstantLoraTxCurrentModel::ConstantLoraTxCurrentModel ()
+{
+  NS_LOG_FUNCTION (this);
+}
+
+ConstantLoraTxCurrentModel::~ConstantLoraTxCurrentModel()
+{
+  NS_LOG_FUNCTION (this);
+}
+
+void
+ConstantLoraTxCurrentModel::SetTxCurrent (double txCurrent)
+{
+  NS_LOG_FUNCTION (this << txCurrent);
+  m_txCurrent = txCurrent;
+}
+
+double
+ConstantLoraTxCurrentModel::GetTxCurrent (void) const
+{
+  return m_txCurrent;
+}
+
+double
+ConstantLoraTxCurrentModel::CalcTxCurrent (double txPowerDbm) const
+{
+  NS_LOG_FUNCTION (this << txPowerDbm);
+  return m_txCurrent;
+}
+
+} // namespace ns3
