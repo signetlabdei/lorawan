673d041b17a475c419c29f410039ac9333131ce7
Alessandro Aimi <alleaimi95@gmail.com>
2023-03-07 11:14:39 +0100

    Fix chirpstack-example.cc

--- examples/chirpstack-example.cc
+++ examples/chirpstack-example.cc
@@ -266,8 +266,7 @@ main(int argc, char* argv[])
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9."
         "eyJhdWQiOiJjaGlycHN0YWNrIiwiaXNzIjoiY2hpcnBzdGFjayIsInN1YiI6IjZlMjQ4NjljLWQxMjItNDZkOS04Nz"
         "E0LTM5Yzc4Nzg4OTRhZCIsInR5cCI6ImtleSJ9.IB20o6Jrcwj5qZ9mPEuthzzqMyc3YNSl8by_ZXrjqhw";
-    csHelper.SetToken(token);
-    csHelper.InitConnection("127.0.0.1", 8090);
+    csHelper.InitConnection("127.0.0.1", 8090, token);
     csHelper.Register(NodeContainer(endDevices, gateways));
 
     // Initialize SF emulating the ADR algorithm, then add variance to path loss
