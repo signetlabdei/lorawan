signetlabdei/lorawan

commit a82f2cc45d00f368991b2f4eb8706abd84e246c0

Update supported ns-3 version to 3.40 (#137)
+ remove obsolete wscript files
+ fix warnings: deprecated Ptr<T>::Tester*() operator

model/periodic-sender.cc
@@ -133,13 +133,13 @@ PeriodicSender::StartApplication (void)
   NS_LOG_FUNCTION (this);
 
   // Make sure we have a MAC layer
-  if (m_mac == 0)
+  if (m_mac)
     {
       // Assumes there's only one device
       Ptr<LoraNetDevice> loraNetDevice = m_node->GetDevice (0)->GetObject<LoraNetDevice> ();
 
       m_mac = loraNetDevice->GetMac ();
-      NS_ASSERT (m_mac != 0);
+      NS_ASSERT (m_mac);
     }
 
   // Schedule the next SendPacket event

NS3-VERSION
@@ -1,1 +1,1 @@
-release ns-3.29
+release ns-3.40

examples/wscript
@@ -1,27 +1,0 @@
-# -*- Mode: python; py-indent-offset: 4; indent-tabs-mode: nil; coding: utf-8; -*-
-
-
-def build(bld):
-    obj = bld.create_ns3_program('simple-network-example', ['lorawan'])
-    obj.source = 'simple-network-example.cc'
-
-    obj = bld.create_ns3_program('network-server-example', ['lorawan'])
-    obj.source = 'network-server-example.cc'
-
-    obj = bld.create_ns3_program('complete-network-example', ['lorawan'])
-    obj.source = 'complete-network-example.cc'
-
-    obj = bld.create_ns3_program('adr-example', ['lorawan'])
-    obj.source = 'adr-example.cc'
-
-    obj = bld.create_ns3_program('lorawan-energy-model-example', ['lorawan'])
-    obj.source = 'lorawan-energy-model-example.cc'
-
-    obj = bld.create_ns3_program('aloha-throughput', ['lorawan'])
-    obj.source = 'aloha-throughput.cc'
-
-    obj = bld.create_ns3_program('parallel-reception-example', ['lorawan'])
-    obj.source = 'parallel-reception-example.cc'
-
-    obj = bld.create_ns3_program('frame-counter-update', ['lorawan'])
-    obj.source = 'frame-counter-update.cc'

helper/forwarder-helper.cc
@@ -79,7 +79,7 @@ ForwarderHelper::InstallPriv (Ptr<Node> node) const
   for (uint32_t i = 0; i < node->GetNDevices (); i++)
     {
       Ptr<NetDevice> currentNetDevice = node->GetDevice (i);
-      if (currentNetDevice->GetObject<LoraNetDevice> () != 0)
+      if (currentNetDevice->GetObject<LoraNetDevice>())
         {
           Ptr<LoraNetDevice> loraNetDevice =
             currentNetDevice->GetObject<LoraNetDevice> ();
@@ -87,7 +87,7 @@ ForwarderHelper::InstallPriv (Ptr<Node> node) const
           loraNetDevice->SetReceiveCallback (MakeCallback
                                                (&Forwarder::ReceiveFromLora, app));
         }
-      else if (currentNetDevice->GetObject<PointToPointNetDevice> () != 0)
+      else if (currentNetDevice->GetObject<PointToPointNetDevice> ())
         {
           Ptr<PointToPointNetDevice> pointToPointNetDevice =
             currentNetDevice->GetObject<PointToPointNetDevice> ();

helper/lora-helper.cc
@@ -57,7 +57,7 @@ NS_LOG_COMPONENT_DEFINE ("LoraHelper");
 
         // Create the PHY
         Ptr<LoraPhy> phy = phyHelper.Create (node, device);
-        NS_ASSERT (phy != 0);
+        NS_ASSERT (phy);
         device->SetPhy (phy);
         NS_LOG_DEBUG ("Done creating the PHY");
 
@@ -104,7 +104,7 @@ NS_LOG_COMPONENT_DEFINE ("LoraHelper");
 
       // Create the MAC
       Ptr<LorawanMac> mac = macHelper.Create (node, device);
-      NS_ASSERT (mac != 0);
+      NS_ASSERT (mac);
       mac->SetPhy (phy);
       NS_LOG_DEBUG ("Done creating the MAC");
       device->SetMac (mac);
@@ -217,10 +217,10 @@ LoraHelper::DoPrintDeviceStatus (NodeContainer endDevices, NodeContainer gateway
     {
       Ptr<Node> object = *j;
       Ptr<MobilityModel> position = object->GetObject<MobilityModel> ();
-      NS_ASSERT (position != 0);
+      NS_ASSERT (position);
       Ptr<NetDevice> netDevice = object->GetDevice (0);
       Ptr<LoraNetDevice> loraNetDevice = netDevice->GetObject<LoraNetDevice> ();
-      NS_ASSERT (loraNetDevice != 0);
+      NS_ASSERT (loraNetDevice);
       Ptr<ClassAEndDeviceLorawanMac> mac = loraNetDevice->GetMac ()->GetObject<ClassAEndDeviceLorawanMac> ();
       int dr = int(mac->GetDataRate ());
       double txPower = mac->GetTransmissionPower ();

helper/lorawan-mac-helper.cc
@@ -77,7 +77,7 @@ LorawanMacHelper::Create (Ptr<Node> node, Ptr<NetDevice> device) const
   mac->SetDevice (device);
 
   // If we are operating on an end device, add an address to it
-  if (m_deviceType == ED_A && m_addrGen != 0)
+  if (m_deviceType == ED_A && m_addrGen)
     {
       mac->GetObject<ClassAEndDeviceLorawanMac> ()->SetDeviceAddress (m_addrGen->NextAddress ());
     }
@@ -461,13 +461,13 @@ LorawanMacHelper::SetSpreadingFactorsUp (NodeContainer endDevices, NodeContainer
     {
       Ptr<Node> object = *j;
       Ptr<MobilityModel> position = object->GetObject<MobilityModel> ();
-      NS_ASSERT (position != 0);
+      NS_ASSERT (position);
       Ptr<NetDevice> netDevice = object->GetDevice (0);
       Ptr<LoraNetDevice> loraNetDevice = netDevice->GetObject<LoraNetDevice> ();
-      NS_ASSERT (loraNetDevice != 0);
+      NS_ASSERT (loraNetDevice);
       Ptr<ClassAEndDeviceLorawanMac> mac =
           loraNetDevice->GetMac ()->GetObject<ClassAEndDeviceLorawanMac> ();
-      NS_ASSERT (mac != 0);
+      NS_ASSERT (mac);
 
       // Try computing the distance from each gateway and find the best one
       Ptr<Node> bestGateway = gateways.Get (0);
@@ -620,13 +620,13 @@ LorawanMacHelper::SetSpreadingFactorsGivenDistribution (NodeContainer endDevices
     {
       Ptr<Node> object = *j;
       Ptr<MobilityModel> position = object->GetObject<MobilityModel> ();
-      NS_ASSERT (position != 0);
+      NS_ASSERT (position);
       Ptr<NetDevice> netDevice = object->GetDevice (0);
       Ptr<LoraNetDevice> loraNetDevice = netDevice->GetObject<LoraNetDevice> ();
-      NS_ASSERT (loraNetDevice != 0);
+      NS_ASSERT (loraNetDevice);
       Ptr<ClassAEndDeviceLorawanMac> mac =
           loraNetDevice->GetMac ()->GetObject<ClassAEndDeviceLorawanMac> ();
-      NS_ASSERT (mac != 0);
+      NS_ASSERT (mac);
 
       double prob = uniformRV->GetValue (0, 1);
 

model/lora-channel.cc
@@ -116,7 +116,7 @@ LoraChannel::Send (Ptr< LoraPhy > sender, Ptr< Packet > packet,
   // Get the mobility model of the sender
   Ptr<MobilityModel> senderMobility = sender->GetMobility ()->GetObject<MobilityModel> ();
 
-  NS_ASSERT (senderMobility != 0);     // Make sure it's available
+  NS_ASSERT (senderMobility);     // Make sure it's available
 
   NS_LOG_INFO ("Starting cycle over all " << m_phyList.size () << " PHYs");
   NS_LOG_INFO ("Sender mobility: " << senderMobility->GetPosition ());
@@ -151,7 +151,7 @@ LoraChannel::Send (Ptr< LoraPhy > sender, Ptr< Packet > packet,
           // Get the id of the destination PHY to correctly format the context
           Ptr<NetDevice> dstNetDevice = m_phyList[j]->GetDevice ();
           uint32_t dstNode = 0;
-          if (dstNetDevice != 0)
+          if (dstNetDevice)
             {
               NS_LOG_INFO ("Getting node index from NetDevice, since it exists");
               dstNode = dstNetDevice->GetNode ()->GetId ();

model/lora-frame-header.h
@@ -328,7 +328,7 @@ LoraFrameHeader::GetMacCommand ()
   std::list< Ptr< MacCommand> >::const_iterator it;
   for (it = m_macCommands.begin (); it != m_macCommands.end (); ++it)
     {
-      if ((*it)->GetObject<T> () != 0)
+      if ((*it)->GetObject<T> ())
         {
           return (*it)->GetObject<T> ();
         }

commit a804320f2e278a52b7d5941cdb90ee73350c65fe

Fix null check in PeriodicSender
Null pointer dereference caused by a refactor in a82f2cc45d00f368991b2f4eb8706abd84e246c0

model/periodic-sender.cc
@@ -135,7 +135,7 @@ PeriodicSender::StartApplication(void)
     NS_LOG_FUNCTION(this);
 
     // Make sure we have a MAC layer
-    if (m_mac)
+    if (!m_mac)
     {
         // Assumes there's only one device
         Ptr<LoraNetDevice> loraNetDevice = m_node->GetDevice(0)->GetObject<LoraNetDevice>();