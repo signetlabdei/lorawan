cd009be22563f441543559dda0094f7bf2b97082
Alessandro Aimi <alleaimi95@gmail.com>
2024-03-04 21:49:07 +0100

    Add licence to test/example files + fix missing copyrights/text in licences

--- examples/lorawan-energy-model-example.cc
+++ examples/lorawan-energy-model-example.cc
@@ -1,4 +1,23 @@
 /*
+ * Copyright (c) 2017 University of Padova
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Davide Magrin <magrinda@dei.unipd.it>
+ */
+
+/*
  * This script simulates a simple network to explain how the Lora energy model
  * works.
  */
--- helper/lora-radio-energy-model-helper.h
+++ helper/lora-radio-energy-model-helper.h
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2017 University of Padova
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
