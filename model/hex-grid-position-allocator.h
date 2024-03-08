f8f1aceee3f8e2569c3a546cd22d5cfbc72459eb
Alessandro Aimi <alleaimi95@gmail.com>
2024-03-08 11:22:50 +0100

    expand compound doc of HexGridPositionAllocator

--- model/hex-grid-position-allocator.h
+++ model/hex-grid-position-allocator.h
@@ -29,14 +29,9 @@ namespace ns3
  * \ingroup lorawan
  * \brief Position allocator for hexagonal tiling
  *
- * Starting starting with a first hexagon in the axes' center, following tiles are added as outward
+ * Starting with a first hexagon in the axes' center, following tiles are added in outward
  * rings. The first position returned for a new ring is always the top one, followed by the others
- * in ant-clockwise rotation.
- *
- * Useful formula:
- *   nGateways = 3 * gatewayRings * gatewayRings - 3 * gatewayRings + 1
- *
- * Representation with 10 gateways, indexed 0-9:
+ * in ant-clockwise rotation. Visual example with 10 tiles, indexed 0-9:
  *                     _____
  *                    /     \
  *              _____/   8   \
@@ -55,6 +50,29 @@ namespace ns3
  *                   \   ˙   /
  *                    \_____/
  *
+ * The size of tiles can be configured by setting the radius \f$\rho_{i}\f$ of the circle
+ * < b>inscribed< \b> within hexagons (i.e., the internal circle).
+ *
+ * Let's say that we are placing access points, and we want to cover a square/circular area with
+ * hexagonal tiles. This leaves us with two questions: (i) which value should we choose for the
+ * internal radius? (ii) how many access point nodes (i.e., tiles) do we need to instantiate?
+ *
+ * For instamce, to guarantee that no point is further than 1km from the center of any tile (i.e.,
+ * to have no uncovered patches), we can choose the radius \f$\rho_{c}\f$ of the
+ * < b>circumscribed< \b> (external) circle of each hexagonal tile to be exactly 1km. Then, question
+ * (i) can be solved by setting the internal radius to \f$\rho_{i}=\frac{\sqrt{3}}{2}\rho_{c}\f$
+ * using the properties of equilateral triangles.
+ *
+ * To understand how many tiles need to be instantiated (that is, in this example, the number of
+ * access point nodes), it is often useful to start from the complete area's diagonal or radius, and
+ * to derive the number of complete rings \f$r\f$ required for coverage. Let's say we want to be
+ * able to cover a distance of at least \f$d\f$km from the center. One way to do this can be to take
+ * the floor of \f$d\f$ divided by the distance between access points (the tiles' centers), which
+ * happens to be \f$2\rho_{i}\f$, and adding one/two ring for good measure. More formally, starting
+ * from a single central tile, we would need at least
+ * \f$r=\left\lfloor\frac{d}{2\rho_{i}}\right\rfloor+1\f$ < b>additional< \b> rings around it. Then,
+ * the total number of tiles \f$n\f$ in a tiling of \f$r\f$ complete rings around a central tile
+ * evaluates to \f$n=3r^{2}-3r+1\f$ providing a possible solution for question (ii).
  */
 class HexGridPositionAllocator : public PositionAllocator
 {
