f36e896e841ae5198eea85108f4a0548ab7bbaaf
Alessandro Aimi <alleaimi95@gmail.com>
2024-03-18 10:45:39 +0100

    document LorawanMacHelper

--- helper/lorawan-mac-helper.h
+++ helper/lorawan-mac-helper.h
@@ -85,6 +85,8 @@ class LorawanMacHelper
 
     /**
      * Set the address generator to use for creation of these nodes.
+     *
+     * \param addrGen pointer to the address generator object
      */
     void SetAddressGenerator(Ptr<LoraDeviceAddressGenerator> addrGen);
 
@@ -97,6 +99,8 @@ class LorawanMacHelper
 
     /**
      * Set the region in which the device is to operate.
+     *
+     * \param region the region enum value
      */
     void SetRegion(enum Regions region);
 
@@ -110,20 +114,72 @@ class LorawanMacHelper
     Ptr<LorawanMac> Create(Ptr<Node> node, Ptr<NetDevice> device) const;
 
     /**
-     * Set up the end device's data rates
-     * This function assumes we are using the following convention:
-     * SF7 -> DR5
-     * SF8 -> DR4
-     * SF9 -> DR3
-     * SF10 -> DR2
-     * SF11 -> DR1
-     * SF12 -> DR0
+     * \brief Initialize the end devices' data rate parameter.
+     *
+     * The DR of each device is set to the maximum possible for its transmissions to be correctly
+     * received by the gateway measuring the best RSSI from the device, mimicking the DR
+     * maximization part of the default online LoRaWAN ADR algorithm. Mind that a single RSSI
+     * measurement between each device and gateway pair is taken for the DR assignment, so the
+     * assignment may be suboptimal in scenarios with a stochastical channel.
+     *
+     * This function uses the following convention (EU868) to compute the transmission range:
+     * DR5 -> SF7
+     * DR4 -> SF8
+     * DR3 -> SF9
+     * DR2 -> SF10
+     * DR1 -> SF11
+     * DR0 -> SF12
+     *
+     * It returns a DR distribution vector with the following counters:
+     * v[0] -> number of devices using DR5
+     * v[1] -> number of devices using DR4
+     * v[2] -> number of devices using DR3
+     * v[3] -> number of devices using DR2
+     * v[4] -> number of devices using DR1
+     * v[5] -> number of devices using DR0, in range of at least a gateway
+     * v[6] -> number of devices using DR0, out of range
+     *
+     * \param endDevices the end devices to configure
+     * \param gateways the gateways to consider for RSSI measurements
+     * \param channel the radio channel to consider for RSSI measurements
+     * \return a vector containing the final number of devices per DR
      */
     static std::vector<int> SetSpreadingFactorsUp(NodeContainer endDevices,
                                                   NodeContainer gateways,
                                                   Ptr<LoraChannel> channel);
+
     /**
-     * Set up the end device's data rates according to the given distribution.
+     * \brief Randomly initialize the end devices' data rate parameter according to the given
+     * distribution.
+     *
+     * This function expects a data rate (DR) distribution vector of length 6 filled with real
+     * numbers summing up to 1. The value at index \f$i\f$ is considered to be the fraction of
+     * devices to be assigned DR \f$5-i\f$, for example:
+     *
+     * distribution[0] == 0.2 -> fraction of devices to be assigned to DR5
+     * distribution[1] == 0.1 -> fraction of devices to be assigned to DR4
+     * distribution[2] == 0.1 -> fraction of devices to be assigned to DR3
+     * distribution[3] == 0.1 -> fraction of devices to be assigned to DR2
+     * distribution[4] == 0.2 -> fraction of devices to be assigned to DR1
+     * distribution[5] == 0.3 -> fraction of devices to be assigned to DR0
+     *
+     * Devices are then randomly assigned a DR following the provided distribution.
+     *
+     * It returns a DR distribution vector with the following counters:
+     * v[0] -> number of devices using DR5
+     * v[1] -> number of devices using DR4
+     * v[2] -> number of devices using DR3
+     * v[3] -> number of devices using DR2
+     * v[4] -> number of devices using DR1
+     * v[5] -> number of devices using DR0
+     *
+     * \todo this function will always cause an array out of bound access error because at one point
+     * it tries to access index 6 of a 6-length vector (cumdistr).
+     *
+     * \param endDevices the end devices to configure
+     * \param gateways \todo unused parameter
+     * \param distribution the distribution (probability mass function) of DR assignment
+     * \return a vector containing the final number of devices per DR
      */
     static std::vector<int> SetSpreadingFactorsGivenDistribution(NodeContainer endDevices,
                                                                  NodeContainer gateways,
@@ -131,54 +187,72 @@ class LorawanMacHelper
 
   private:
     /**
-     * Perform region-specific configurations for the 868 MHz EU band.
+     * \brief Perform region-specific configurations for the 868 MHz EU band.
+     *
+     * \param edMac pointer to the device MAC layer to configure
      */
     void ConfigureForEuRegion(Ptr<ClassAEndDeviceLorawanMac> edMac) const;
 
     /**
-     * Perform region-specific configurations for the 868 MHz EU band.
+     * \brief Perform region-specific configurations for the 868 MHz EU band.
+     *
+     * \param gwMac pointer to the gateway MAC layer to configure
      */
     void ConfigureForEuRegion(Ptr<GatewayLorawanMac> gwMac) const;
 
     /**
-     * Apply configurations that are common both for the GatewayLorawanMac and the
+     * \brief Apply configurations that are common both for the GatewayLorawanMac and the
      * ClassAEndDeviceLorawanMac classes.
+     *
+     * \param lorawanMac pointer to the MAC layer to configure
      */
     void ApplyCommonEuConfigurations(Ptr<LorawanMac> lorawanMac) const;
 
     /**
-     * Perform region-specific configurations for the SINGLECHANNEL band.
+     * \brief Perform region-specific configurations for the SINGLECHANNEL band.
+     *
+     * \param edMac pointer to the device MAC layer to configure
      */
     void ConfigureForSingleChannelRegion(Ptr<ClassAEndDeviceLorawanMac> edMac) const;
 
     /**
-     * Perform region-specific configurations for the SINGLECHANNEL band.
+     * \brief Perform region-specific configurations for the SINGLECHANNEL band.
+     *
+     * \param gwMac pointer to the gateway MAC layer to configure
      */
     void ConfigureForSingleChannelRegion(Ptr<GatewayLorawanMac> gwMac) const;
 
     /**
-     * Apply configurations that are common both for the GatewayLorawanMac and the
+     * \brief Apply configurations that are common both for the GatewayLorawanMac and the
      * ClassAEndDeviceLorawanMac classes.
+     *
+     * \param lorawanMac pointer to the MAC layer to configure
      */
     void ApplyCommonSingleChannelConfigurations(Ptr<LorawanMac> lorawanMac) const;
 
     /**
-     * Perform region-specific configurations for the ALOHA band.
+     * \brief Perform region-specific configurations for the ALOHA band.
+     *
+     * \param edMac pointer to the device MAC layer to configure
      */
     void ConfigureForAlohaRegion(Ptr<ClassAEndDeviceLorawanMac> edMac) const;
 
     /**
-     * Perform region-specific configurations for the ALOHA band.
+     * \brief Perform region-specific configurations for the ALOHA band.
+     *
+     * \param gwMac pointer to the gateway MAC layer to configure
      */
     void ConfigureForAlohaRegion(Ptr<GatewayLorawanMac> gwMac) const;
 
     /**
-     * Apply configurations that are common both for the GatewayLorawanMac and the
+     * \brief Apply configurations that are common both for the GatewayLorawanMac and the
      * ClassAEndDeviceLorawanMac classes.
+     *
+     * \param lorawanMac pointer to the MAC layer to configure
      */
     void ApplyCommonAlohaConfigurations(Ptr<LorawanMac> lorawanMac) const;
 
-    ObjectFactory m_mac;
+    ObjectFactory m_mac;                       //!< Mac layer object factory
     Ptr<LoraDeviceAddressGenerator> m_addrGen; //!< Pointer to the address generator to use
     enum DeviceType m_deviceType;              //!< The kind of device to install
     enum Regions m_region;                     //!< The region in which the device will operate
